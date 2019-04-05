8bdf4924b828f09967f6fc8872edd49f8e09ed70
Antavio <njuguna13@gmail.com>
2019-04-05 14:09:57 +0300

    Add a class instance to the user class.

--- user.py
+++ user.py
@@ -2,7 +2,9 @@ class User:
     """
     Class that generates new instances of users
     """
-    def __init__(self,username,password):
+    def __init__(self,firstname,secondname,username,password):
+
+        users_list = []
 
         '''
         __init__ method that helps us define properties for our objects.
@@ -12,5 +14,7 @@ class User:
             password : New user password.
         '''
 
+        self.firstname = firstname
+        self.secondname = secondname
         self.username = username
         self.password = password
